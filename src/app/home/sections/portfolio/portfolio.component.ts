a3056d906b8936f13f28d7c81fcda444d70fbd10
Ghadi <yobiz668@gmail.com>
2023-06-25 14:33:02 +0300

    fixes

--- src/app/home/sections/portfolio/portfolio.component.ts
+++ src/app/home/sections/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component , ViewChild } from '@angular/core';
+import { Component , ViewChild , HostBinding , HostListener, OnInit  } from '@angular/core';
 import { DragScrollComponent } from 'ngx-drag-scroll';
 
 @Component({
@@ -7,15 +7,35 @@ import { DragScrollComponent } from 'ngx-drag-scroll';
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent {
+  isScrollbarHidden: boolean = true;
+
+  i : number = 1
 
   @ViewChild('navv', { read: DragScrollComponent }) ds!: DragScrollComponent;
 
     moveLeft() {
         this.ds.moveLeft();
+        if (this.i > 1) {
+          this.i--
+        }
     }
 
     moveRight() {
         this.ds.moveRight();
+        if (this.i >= 1 && this.i < 2){
+          this.i++
+        }
     }
+
+    @HostListener('window:resize')
+      ngOnInit() {
+        const windowWidth = window.innerWidth;
+        if (windowWidth >= 768) {
+          this.isScrollbarHidden = true
+        }
+        else if (windowWidth < 768){
+          this.isScrollbarHidden = false;
+        }
+      }
     
 }
