6811a809528ca19cb211cacced9ebd4a42ef7630
Ghadi <yobiz668@gmail.com>
2023-06-26 10:05:01 +0300

    ..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './home/sections/about/about.component';
+import { ContactComponent } from './home/sections/contact/contact.component';
+import { PortfolioComponent } from './home/sections/portfolio/portfolio.component';
+import { MainTopComponent } from './home/sections/main-top/main-top.component';
+
+const routes: Routes = [
+  { path: 'home', component: MainTopComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
