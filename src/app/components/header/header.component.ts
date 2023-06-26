0d2ddfb42ab2937976f8e2492a395df744d9e464
Ghadi <yobiz668@gmail.com>
2023-06-26 18:59:55 +0300

    added scroll feature

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , ElementRef ,ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+  constructor(private elementRef: ElementRef) {}
+
   open : boolean = false;
+
+  scrollTo(elementId: string): void {
+    if (elementId === 'home'){
+      scroll({ top: 0,
+        left: 0,
+        behavior: 'smooth'});
+    }
+    if (elementId === 'about'){
+      scroll({ top: 1230,
+        left: 0,
+        behavior: 'smooth'});
+    }
+    if (elementId === 'portfolio'){
+      scroll({ top: 2650,
+        left: 0,
+        behavior: 'smooth'});
+    }
+    if (elementId === 'contact'){
+      scroll({ top: 3400,
+        left: 0,
+        behavior: 'smooth'});
+    }
+      this.open = false
+  }
 
   toggleMenu() :void {
     this.open = !this.open
