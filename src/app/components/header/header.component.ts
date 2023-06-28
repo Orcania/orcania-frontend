9fa66a5905601e36b187b92b9a6f7c787d391d7d
Ghadi <yobiz668@gmail.com>
2023-06-28 18:43:38 +0300

    working on the portfolio view on mobile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -13,36 +13,6 @@ export class HeaderComponent {
   open : boolean = false;
   link : string = 'home';
 
-  /*scrollTo(elementId: string): void {
-    if (elementId === 'home'){
-      this.link = 'home';
-      scroll({ top: 0,
-        left: 0,
-        behavior: 'smooth'});
-    }
-
-    else if (elementId === 'about'){
-      this.link = 'about';
-      scroll({ top: 1230,
-        left: 0,
-        behavior: 'smooth'});
-    }
-
-    else if (elementId === 'portfolio'){
-      this.link = 'portfolio';
-      scroll({ top: 2650,
-        left: 0,
-        behavior: 'smooth'});
-    }
-
-    else if (elementId === 'contact'){
-      this.link = 'contact';
-      scroll({ top: 3400,
-        left: 0,
-        behavior: 'smooth'});
-    }
-      this.open = false
-  } */
 
    scrollTo(elementId: string): void {
     if (elementId === 'home'){
